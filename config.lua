36e4e5408589dc652017033048724b67c66f347c
RollHimOut <134718286+RollHimOut@users.noreply.github.com>
2024-11-18 05:45:45 -0500

    created config.lua snippet

---
+++ config.lua
@@ -0,0 +1,9 @@
+-- Grapeseed Delivery Locations (add in between two other locations in the config.lua
+
+{
+        vector3(1683.05, 4689.6, 43.07),
+		    vector3(1673.91, 4658.26, 43.37),
+		    vector3(1718.88, 4677.26, 43.66),
+		    vector3(1663.95, 4739.68, 42.01),
+		    vector3(1662.35, 4776.2, 42.01),
+    },
